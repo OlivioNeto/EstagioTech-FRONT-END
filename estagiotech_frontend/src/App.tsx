2fde14ffe64993211044cac948ac54d8cff4fc6e
Olivio Neto <netoolivio34@gmail.com>
2023-11-17 10:55:10 -0300

    Atualizando as rotas

--- estagiotech_frontend/src/App.tsx
+++ estagiotech_frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Admin from './pages/Admin'
+import Page404 from './pages/Page404'
 
 function App() {
 
   return (
-    <h1>EstagioTech</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/adm' element={<Login/>}/>
+          <Route path='/adm/admin' element={<Admin/>}/>
+          <Route path='/*' element={<Page404/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
