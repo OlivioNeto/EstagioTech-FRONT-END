9600042fe5e2378a8967494e6317e687cf9ea918
julioBarbosa33 <barbosajulio131@gmail.com>
2024-10-03 14:49:21 -0300

    Concertando alguns erros presentes na tela DocumentoVersao faltando apenas a definir rota

--- estagiotech_frontend/src/pages/Admin/DocumentoVersao/TableDocumentoVersao/cadastro/form-cad-DocumentoVersao.tsx
+++ estagiotech_frontend/src/pages/Admin/DocumentoVersao/TableDocumentoVersao/cadastro/form-cad-DocumentoVersao.tsx
@@ -1,3 +1,170 @@
+// import { useNavigate } from "react-router-dom";
+// import * as z from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { Combobox, ComboboxProps } from "@/components/ui/combobox";
+// import { Button } from "@/components/ui/button";
+// import api from "../../../../../service/api";
+// import { Card, CardContent, CardFooter } from "@/components/ui/card";
+// import { useEffect, useState } from "react";
+// import { DocumentoVersaoProps } from "../table/columms";
+
+// export type DocumentoVersaoProps = {
+//   idDocumentoVersao: number;
+//   comentario: string;
+//   anexo: number;
+//   data: string;
+//   situacao: string;
+//   idDocumento: number;
+//   key: number;
+// }
+
+
+// const formSchema = z.object({
+//   nomeSupervisor: z.string(),
+//   concedenteId: z.number(),
+// });
+
+// type DocumentoVersaoProps = z.infer<typeof formSchema>;
+
+// const DocumentoVersaoProps = ({ data }: { data: DocumentoVersaoProps }) => {
+//   const navigate = useNavigate();
+
+//   const [isEdit, setIsEdit] = useState(false);
+//   const [nomeSupervisor, setnomeSupervisor] = useState("") 
+//   const [dataComboBoxC, setDataComboBoxC] = useState<ComboboxProps[]>([]);
+//   const [valueComboBoxC, setValueComboBoxC] = useState("");
+
+//   const form = useForm<DocumentoVersaoProps>({
+//     resolver: zodResolver(formSchema),
+//     values: {
+//       idDocumentoVersao: "",
+//       comentario: "", 
+//       anexo:""
+//       data: 0,
+//       situacao: "", 
+//       idDocumento: 0,
+//       key:0,
+//     },
+//     defaultValues: {
+//       idDocumentoVersao: "",
+//       comentario: "", 
+//       anexo:"",
+//       data: 0,
+//       situacao: "", 
+//       idDocumento: 0,
+//       key:0,
+//     },
+//   });
+
+//   useEffect(() => {
+//     (async () => {
+//       const concedenteSelecionado = data.concedenteId;
+//       const checkIsedit = Object.keys(data).length;
+//       if (concedenteSelecionado > 0) setIsEdit(true);
+//       if (concedenteSelecionado) {
+//         setValueComboBoxC(concedenteSelecionado.toString()),
+//         setnomeSupervisor(data.nomeSupervisor);
+//       }
+
+//       const resp: ConcendenteProps[] = (await api.get("/concedente")).data;
+
+//       setDataComboBoxC(
+//         resp.map((item) => {
+//           return {
+//             value: item.concedenteId.toString(),
+//             label: item.razaoSocial,
+//           };
+//         })
+//       );
+//     })();
+//   }, [data]);
+
+//   async function onSubmit(values: FormCadastroProps) {
+//     console.log(values)
+//    ! isEdit ?
+//       await api
+//         .post("/SupervisorEstagio", {
+//           concedenteId: valueComboBoxC, nomeSupervisor: nomeSupervisor
+//         })
+//         .finally(() => navigate("/adm/supervisorestagio"))
+//       : await api
+//         .put(`/SupervisorEstagio/${data.idSupervisor}`, {
+//           idSupervisor: data.idSupervisor,
+//           nomeSupervisor: nomeSupervisor,
+//           concedenteId: valueComboBoxC,
+//         })
+//         .finally(() => navigate("/adm/supervisorestagio"));
+//   }
+
+//   return (
+//     <Card className="p-4">
+//       <Form {...form}>
+//         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//           <CardContent>
+//             <FormField
+//               key="nomeSupervisor" // Adicionando chave única aqui
+//               control={form.control}
+//               name="nomeSupervisor"
+//               render={({ field }) => (
+//                 <FormItem className="mt-5">
+//                   <FormLabel>Nome do Supervisor</FormLabel>
+//                   <FormControl>
+//                     <Input placeholder="Nome do Supervisor"onChange={(e)=> setnomeSupervisor(e.target.value)} value = {nomeSupervisor} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+
+//             <FormField
+//               key="concedenteId" // Adicionando chave única aqui
+//               control={form.control}
+//               name="concedenteId"
+//               render={({ field }) => (
+//                 <FormItem className="flex items-center gap-4 mt-5">
+//                   <FormLabel>Nome do concedente</FormLabel>
+//                   <FormControl>
+//                     <Combobox
+//                       data={dataComboBoxC}
+//                       value={valueComboBoxC}
+//                       setValue={setValueComboBoxC}
+//                     />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//           </CardContent>
+
+//           <CardFooter className="flex gap-4">
+//             <Button type="submit">
+//               {!isEdit ? "Cadastrar" : "Salvar alterações"}
+//             </Button>
+//             <Button
+//               type="button"
+//               variant="secondary"
+//               onClick={() => navigate("/adm/supervisorestagio")}
+//             >
+//               Voltar
+//             </Button>
+//           </CardFooter>
+//         </form>
+//       </Form>
+//     </Card>
+//   );
+// };
+
+// export default FormCadastroSupervisorEstagio;
 import { useNavigate } from "react-router-dom";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -16,7 +183,6 @@ import { Button } from "@/components/ui/button";
 import api from "../../../../../service/api";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { useEffect, useState } from "react";
-import { DocumentoVersaoProps } from "../table/columms";
 
 export type DocumentoVersaoProps = {
   idDocumentoVersao: number;
@@ -26,84 +192,73 @@ export type DocumentoVersaoProps = {
   situacao: string;
   idDocumento: number;
   key: number;
-}
-
+};
 
 const formSchema = z.object({
-  nomeSupervisor: z.string(),
-  concedenteId: z.number(),
+  comentario: z.string(),
+  anexo: z.number(),
+  idDocumento: z.number(),
 });
 
-type DocumentoVersaoProps = z.infer<typeof formSchema>;
-
-const DocumentoVersaoProps = ({ data }: { data: DocumentoVersaoProps }) => {
+const DocumentoVersaoForm = ({ data }: { data: DocumentoVersaoProps }) => {
   const navigate = useNavigate();
 
   const [isEdit, setIsEdit] = useState(false);
-  const [nomeSupervisor, setnomeSupervisor] = useState("") 
-  const [dataComboBoxC, setDataComboBoxC] = useState<ComboboxProps[]>([]);
-  const [valueComboBoxC, setValueComboBoxC] = useState("");
+  const [comentario, setComentario] = useState("");
+  const [dataComboBoxD, setDataComboBoxD] = useState<ComboboxProps[]>([]);
+  const [valueComboBoxD, setValueComboBoxD] = useState("");
 
-  const form = useForm<DocumentoVersaoProps>({
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    values: {
-      idDocumentoVersao: "",
-      comentario: "", 
-      anexo:""
-      data: 0,
-      situacao: "", 
-      idDocumento: 0,
-      key:0,
-    },
     defaultValues: {
-      idDocumentoVersao: "",
-      comentario: "", 
-      anexo:"",
-      data: 0,
-      situacao: "", 
+      comentario: "",
+      anexo: 0,
       idDocumento: 0,
-      key:0,
     },
   });
 
   useEffect(() => {
     (async () => {
-      const concedenteSelecionado = data.concedenteId;
-      const checkIsedit = Object.keys(data).length;
-      if (concedenteSelecionado > 0) setIsEdit(true);
-      if (concedenteSelecionado) {
-        setValueComboBoxC(concedenteSelecionado.toString()),
-        setnomeSupervisor(data.nomeSupervisor);
+      if (data) {
+        const documentoSelecionado = Object.keys(data).length;
+        if (documentoSelecionado > 0) setIsEdit(true);
+        if (documentoSelecionado) {
+          setValueComboBoxD(documentoSelecionado.toString());
+          setComentario(data.comentario);
+        }
       }
 
-      const resp: ConcendenteProps[] = (await api.get("/concedente")).data;
+      const resp: DocumentoVersaoProps[] = (await api.get("/documento")).data;
 
-      setDataComboBoxC(
-        resp.map((item) => {
-          return {
-            value: item.concedenteId.toString(),
-            label: item.razaoSocial,
-          };
-        })
+      setDataComboBoxD(
+        resp.map((item) => ({
+          value: item.idDocumentoVersao.toString(),
+          label: item.idDocumento.toString(),
+        }))
       );
     })();
   }, [data]);
 
-  async function onSubmit(values: FormCadastroProps) {
-    console.log(values)
-   ! isEdit ?
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    if (!isEdit) {
       await api
-        .post("/SupervisorEstagio", {
-          concedenteId: valueComboBoxC, nomeSupervisor: nomeSupervisor
+        .post("/DocumentoVersao", {
+          idDocumento: valueComboBoxD,
+          comentario: comentario,
+          anexo: values.anexo,
         })
-        .finally(() => navigate("/adm/supervisorestagio"))
-      : await api
-        .put(`/SupervisorEstagio/${data.idSupervisor}`, {
-          idSupervisor: data.idSupervisor,
-          nomeSupervisor: nomeSupervisor,
-          concedenteId: valueComboBoxC,
+        .finally(() => navigate("/adm/documentoversao"));
+    } else {
+      await api
+        .put(`/DocumentoVersao/${data.idDocumentoVersao}`, {
+          idDocumentoVersao: data.idDocumentoVersao,
+          comentario: comentario,
+          anexo: values.anexo,
+          idDocumento: valueComboBoxD,
         })
-        .finally(() => navigate("/adm/supervisorestagio"));
+        .finally(() => navigate("/adm/documentoversao"));
+    }
   }
 
   return (
@@ -112,14 +267,18 @@ const DocumentoVersaoProps = ({ data }: { data: DocumentoVersaoProps }) => {
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <CardContent>
             <FormField
-              key="nomeSupervisor" // Adicionando chave única aqui
               control={form.control}
-              name="nomeSupervisor"
+              name="comentario"
               render={({ field }) => (
                 <FormItem className="mt-5">
-                  <FormLabel>Nome do Supervisor</FormLabel>
+                  <FormLabel>Comentário</FormLabel>
                   <FormControl>
-                    <Input placeholder="Nome do Supervisor"onChange={(e)=> setnomeSupervisor(e.target.value)} value = {nomeSupervisor} />
+                    <Input
+                      placeholder="Comentário"
+                      {...field}
+                      value={comentario}
+                      onChange={(e) => setComentario(e.target.value)}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -127,17 +286,30 @@ const DocumentoVersaoProps = ({ data }: { data: DocumentoVersaoProps }) => {
             />
 
             <FormField
-              key="concedenteId" // Adicionando chave única aqui
               control={form.control}
-              name="concedenteId"
+              name="anexo"
+              render={({ field }) => (
+                <FormItem className="mt-5">
+                  <FormLabel>Anexo</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="idDocumento"
               render={({ field }) => (
                 <FormItem className="flex items-center gap-4 mt-5">
-                  <FormLabel>Nome do concedente</FormLabel>
+                  <FormLabel>Documento</FormLabel>
                   <FormControl>
                     <Combobox
-                      data={dataComboBoxC}
-                      value={valueComboBoxC}
-                      setValue={setValueComboBoxC}
+                      data={dataComboBoxD}
+                      value={valueComboBoxD}
+                      setValue={setValueComboBoxD}
                     />
                   </FormControl>
                   <FormMessage />
@@ -153,7 +325,7 @@ const DocumentoVersaoProps = ({ data }: { data: DocumentoVersaoProps }) => {
             <Button
               type="button"
               variant="secondary"
-              onClick={() => navigate("/adm/supervisorestagio")}
+              onClick={() => navigate("/adm/documentoversao")}
             >
               Voltar
             </Button>
@@ -164,4 +336,5 @@ const DocumentoVersaoProps = ({ data }: { data: DocumentoVersaoProps }) => {
   );
 };
 
-export default FormCadastroSupervisorEstagio;
+export default DocumentoVersaoForm;
+
--- estagiotech_frontend/src/pages/Admin/DocumentoVersao/TableDocumentoVersao/cadastro/index.tsx
+++ estagiotech_frontend/src/pages/Admin/DocumentoVersao/TableDocumentoVersao/cadastro/index.tsx
@@ -1,41 +1,225 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import CadastroDocumentoNecessario from "./form-cad-DocumentoNecessario";
-import { DocumentoNecessarioProps } from "../table/columns";
-import api from "@/service/api";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { DocumentoVersaoProps } from "./form-cad-DocumentoVersao";
+// import { DocumentoVersaoProps } from "../table/columms";
+// import api from "@/service/api";
+// import DocumentoVersaoForm from "./form-cad-DocumentoVersao";
 
-export default function CadDocumentoNecessario() {
-    const [documentoNecessario, setdocumentoNecessario] = useState<DocumentoNecessarioProps>({idDocumentoNecessario: 0, idTipoDocumento: 0, descricaoTipoEstagio: "", idTipoEstagio: 0, descricaoTipoDocumento: "", key: 0});
-    const { id } = useParams();
-    useEffect(() => {
-      console.log(id)
-    }, [id])
+// export default function CadDocumentove() {
+//     const [documentoNecessario, setdocumentoNecessario] = useState<DocumentoVersaoProps>({idDocumentoVersao: 0, idDocumento: 0, comentario: "", anexo: 0, data: "", situacao:"" ,key 0});
+//     const { id } = useParams();
+//     useEffect(() => {
+//       console.log(id)
+//     }, [id])
     
-    useEffect(() => {
-        (async () => {
-            if (id) {
-                const data = (await api.get(`/DocumentoNecessario/${id}`)).data;
-                setdocumentoNecessario(data);
-            } else {
-                setdocumentoNecessario({
-                    idDocumentoNecessario: 0,
-                    idTipoDocumento: 0,
-                    descricaoTipoEstagio: "", 
-                    idTipoEstagio: 0,
-                    descricaoTipoDocumento: "",
-                    key: 0
-                });
-            }
-            return;
-        })();
-    }, []);
+//     useEffect(() => {
+//         (async () => {
+//             if (id) {
+//                 const data = (await api.get(`/DocumentoNecessario/${id}`)).data;
+//                 setdocumentoNecessario(data);
+//             } else {
+//                 setdocumentoNecessario({
+//                     idDocumento: 0,
+//                     idDocumentoVersao: 0,
+//                     anexo: 0 , 
+//                     data: 0,
+//                     // situacao: "",
+//                     key: 0,
+//                 });
+//             }
+//             return;
+//         })();
+//     }, []);
 
-    return (
-        <div>
-            <p className="text-2xl mb-4">Cadastro do Documento Necessário</p>
-            <CadastroDocumentoNecessario idDocumentoNecessario={documentoNecessario.idDocumentoNecessario}
-            idTipoEstagio={documentoNecessario.idTipoDocumento}
-            idTipoDocumento={documentoNecessario.idTipoEstagio}/> 
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             <p className="text-2xl mb-4">Cadastro do Documento Versao</p>
+//             <CadastroDocumento i={DocumentoVersaoForm.idDocumentoVersao}
+//             idTipoEstagio={documentoNecessario.idTipoDocumento}
+//             idTipoDocumento={documentoNecessario.idTipoEstagio}/> 
+//         </div>
+//     );
+// }
+import { useNavigate } from "react-router-dom";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Combobox, ComboboxProps } from "@/components/ui/combobox";
+import { Button } from "@/components/ui/button";
+import api from "../../../../../service/api";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+
+// Definição do tipo DocumentoVersaoProps para representar as versões de documentos
+export type DocumentoVersaoProps = {
+  idDocumentoVersao: number;
+  comentario: string;
+  anexo: number;
+  data: string;
+  situacao: string;
+  idDocumento: number;
+  key: number;
+};
+
+// Definindo o esquema de validação para o formulário com Zod
+const formSchema = z.object({
+  comentario: z.string().min(1, "O comentário é obrigatório"),
+  anexo: z.number().min(1, "O anexo é obrigatório"),
+  idDocumento: z.number().min(1, "O documento é obrigatório"),
+});
+
+const DocumentoVersaoForm = ({ data }: { data: DocumentoVersaoProps }) => {
+  const navigate = useNavigate();
+
+  // Estado para verificar se é uma edição ou um novo cadastro
+  const [isEdit, setIsEdit] = useState(false);
+  const [comentario, setComentario] = useState("");
+  const [dataComboBoxD, setDataComboBoxD] = useState<ComboboxProps[]>([]);
+  const [valueComboBoxD, setValueComboBoxD] = useState("");
+
+  // Inicialização do hook useForm para gerenciamento do formulário
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      comentario: "",
+      anexo: 0,
+      idDocumento: 0,
+    },
+  });
+
+  // useEffect para carregar dados ao montar o componente
+  useEffect(() => {
+    (async () => {
+      if (data) {
+        const documentoSelecionado = data.idDocumento;
+        if (documentoSelecionado > 0) setIsEdit(true);
+        if (documentoSelecionado) {
+          setValueComboBoxD(documentoSelecionado.toString());
+          setComentario(data.comentario);
+        }
+      }
+
+      const resp: DocumentoVersaoProps[] = (await api.get("/documento")).data;
+
+      setDataComboBoxD(
+        resp.map((item) => ({
+          value: item.idDocumento.toString(),
+          label: item.comentario.toString(),
+        }))
+      );
+    })();
+  }, [data]);
+
+  // Função de submissão do formulário, realizando POST ou PUT conforme o caso
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    if (!isEdit) {
+      await api
+        .post("/DocumentoVersao", {
+          idDocumento: valueComboBoxD,
+          comentario: comentario,
+          anexo: values.anexo,
+        })
+        .finally(() => navigate("/adm/documentoversao"));
+    } else {
+      await api
+        .put(`/DocumentoVersao/${data.idDocumentoVersao}`, {
+          idDocumentoVersao: data.idDocumentoVersao,
+          comentario: comentario,
+          anexo: values.anexo,
+          idDocumento: valueComboBoxD,
+        })
+        .finally(() => navigate("/adm/documentoversao"));
+    }
+  }
+
+  // Renderização do formulário
+  return (
+    <Card className="p-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <CardContent>
+            {/* Campo para o Comentário */}
+            <FormField
+              control={form.control}
+              name="comentario"
+              render={({ field }) => (
+                <FormItem className="mt-5">
+                  <FormLabel>Comentário</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Comentário"
+                      {...field}
+                      value={comentario}
+                      onChange={(e) => setComentario(e.target.value)}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Campo para o Anexo */}
+            <FormField
+              control={form.control}
+              name="anexo"
+              render={({ field }) => (
+                <FormItem className="mt-5">
+                  <FormLabel>Anexo</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Combobox para Selecionar Documento */}
+            <FormField
+              control={form.control}
+              name="idDocumento"
+              render={({ field}) => (
+                <FormItem className="flex items-center gap-4 mt-5">
+                  <FormLabel>Documento</FormLabel>
+                  <FormControl>
+                    <Combobox
+                      data={dataComboBoxD}
+                      value={valueComboBoxD}
+                      setValue={setValueComboBoxD}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+
+          {/* Botões de Ação */}
+          <CardFooter className="flex gap-4">
+            <Button type="submit">
+              {!isEdit ? "Cadastrar" : "Salvar alterações"}
+            </Button>
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={() => navigate("/adm/documentoversao")}
+            >
+              Voltar
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+};
+
+export default DocumentoVersaoForm;
