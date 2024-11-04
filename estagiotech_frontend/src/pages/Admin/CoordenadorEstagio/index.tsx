3f64144637651a1ca28444c9e16925d00a9f2c05
Olivio Neto <netoolivio34@gmail.com>
2024-11-04 11:21:04 -0300

    Corrgindo os erros nas páginas concedentes e coordenador estágio

--- estagiotech_frontend/src/pages/Admin/CoordenadorEstagio/index.tsx
+++ estagiotech_frontend/src/pages/Admin/CoordenadorEstagio/index.tsx
@@ -1,24 +1,35 @@
 import { useEffect, useState } from "react";
-import { CoordenadorEstagioProps, columns } from "./TableCoordenadorEstagio/table/columns";
+import { columns } from "./TableCoordenadorEstagio/table/columns";
 import { DataTable } from "../../../components/data-table";
 import api from "@/service/api";
 import { Button } from "@/components/ui/button";
 import { PlusCircleIcon, PrinterIcon } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import { ColumnDef } from "@tanstack/react-table";
+
+// Tipo para Coordenador de Estágio
+export type CoordenadorEstagioProps = {
+  idCoordenadorEstagio: number;
+  nomeCoordenador: string;
+  dataCadastro: string;
+  status: boolean;
+  key: number;
+};
 
 export default function CoordenadorEstagio() {
   const [data, setData] = useState<CoordenadorEstagioProps[]>([]);
 
   useEffect(() => {
     (async () => {
-      const dataCoordenadorEstagio: CoordenadorEstagioProps[] = await (
+      const data: CoordenadorEstagioProps[] = await (
         await api.get("/CoordenadorEstagio")
       ).data;
 
-      const includeKeyData = dataCoordenadorEstagio.map((item) => {
-        return { ...item, key: item.idCoordenadorEstagio };
+      // Certifique-se de adicionar a propriedade 'key' aos itens
+      const includeKeyData = data.map((item, idx) => {
+        return { ...item, key: idx };
       });
-      console.log(includeKeyData)
+
       setData(includeKeyData);
     })();
   }, []);
@@ -31,14 +42,15 @@ export default function CoordenadorEstagio() {
       <div className="flex gap-2">
         <NavLink to="/adm/coordenadorestagio/cadastro">
           <Button variant="secondary" className="gap-2">
-            <PlusCircleIcon /> Novo Coordenador de Estagio
+            <PlusCircleIcon /> Novo Coordenador de Estágio
           </Button>
         </NavLink>
         <Button variant="secondary" className="gap-2">
           <PrinterIcon /> Imprimir
         </Button>
       </div>
-      <DataTable columns={columns} data={data} />
+      {/* Usando 'CoordenadorEstagioProps' para tipagem correta */}
+      <DataTable columns={columns as ColumnDef<CoordenadorEstagioProps>[]} data={data} searchColumnKey={"key"} />
     </div>
   );
 }
